6adf88782125c0309d40d66d92dc985d4f5a1403
effilin <effienummer@gmail.com>
2024-09-11 11:07:23 -0400

    install bootstrap pondstart

--- around-the-pond/src/app/page.tsx
+++ around-the-pond/src/app/page.tsx
@@ -2,4 +2,15 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  return(
+    <div>
+      <div className="sky">
+
+      </div>
+      <div className="pond">
+
+      </div>
+    </div>
+  )
 }
